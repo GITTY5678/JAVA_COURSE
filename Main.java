6440cffe12ce793bec5c90ea41f14ff481737f1a
GITTY5678 <rge7311@gmail.com>
2025-07-09 17:00:07 +0530

    reverse over

--- Main.java
+++ Main.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main (String[] args){
-        System.out.println("Student test");
         Scanner scanner = new Scanner (System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-        System.out.print("Enter your height:");
-        double height = scanner.nextDouble();
-        if (age <18) {
-            System.out.println("You are a minor.");
-        } else {
-            System.out.println("You are an adult.");
+        int n = scanner.nextInt();
+        int reversed=0;
+        while(n!=0){
+            int digit=n%10;
+            reversed=reversed*10 + digit;
+            n=n/10;
+
         }
+        System.out.println(reversed);
+        scanner.close();
+
+        
+
 
         
     }
